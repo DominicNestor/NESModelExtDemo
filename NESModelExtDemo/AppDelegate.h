2aa7dad7c9492b076ae213abae6a863864540166
Nestor <admin@nestor.me>
2015-04-09 10:53:17 +0800

    Initial Commit

---
+++ NESModelExtDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NESModelExtDemo
+//
+//  Created by Nestor on 15/4/9.
+//  Copyright (c) 2015年 NesTalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
